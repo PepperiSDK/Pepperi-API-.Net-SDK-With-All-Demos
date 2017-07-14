eca8cc8ea60a2848ea9c4ccf2d303522a1db3784
Yossi Rotlevy, VP Data Integration & Co-Founder <yossi.r@pepperi.com>
2017-07-14 12:57:43 +0300

    Added C# WinForms Demo Project
    Added C# WinForms Demo Project

---
+++ Pepperi.Demo.WinForms/frmSample.Designer.cs
@@ -0,0 +1,220 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class frmSample
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnViewItems = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnViewAcounts = new System.Windows.Forms.Button();
+            this.btnSaveAll = new System.Windows.Forms.Button();
+            this.btnSaveSelected = new System.Windows.Forms.Button();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtToken = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtFilter = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnViewItems
+            // 
+            this.btnViewItems.Location = new System.Drawing.Point(12, 89);
+            this.btnViewItems.Name = "btnViewItems";
+            this.btnViewItems.Size = new System.Drawing.Size(154, 64);
+            this.btnViewItems.TabIndex = 0;
+            this.btnViewItems.Text = "View Items";
+            this.btnViewItems.UseVisualStyleBackColor = true;
+            this.btnViewItems.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 162);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(1154, 389);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // btnViewAcounts
+            // 
+            this.btnViewAcounts.Location = new System.Drawing.Point(193, 89);
+            this.btnViewAcounts.Name = "btnViewAcounts";
+            this.btnViewAcounts.Size = new System.Drawing.Size(205, 64);
+            this.btnViewAcounts.TabIndex = 2;
+            this.btnViewAcounts.Text = "View Accounts (Customers)";
+            this.btnViewAcounts.UseVisualStyleBackColor = true;
+            this.btnViewAcounts.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btnSaveAll
+            // 
+            this.btnSaveAll.Location = new System.Drawing.Point(1012, 89);
+            this.btnSaveAll.Name = "btnSaveAll";
+            this.btnSaveAll.Size = new System.Drawing.Size(154, 64);
+            this.btnSaveAll.TabIndex = 3;
+            this.btnSaveAll.Text = "Save All";
+            this.btnSaveAll.UseVisualStyleBackColor = true;
+            this.btnSaveAll.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // btnSaveSelected
+            // 
+            this.btnSaveSelected.Location = new System.Drawing.Point(852, 89);
+            this.btnSaveSelected.Name = "btnSaveSelected";
+            this.btnSaveSelected.Size = new System.Drawing.Size(154, 64);
+            this.btnSaveSelected.TabIndex = 4;
+            this.btnSaveSelected.Text = "Save Selected";
+            this.btnSaveSelected.UseVisualStyleBackColor = true;
+            this.btnSaveSelected.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(12, 12);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(219, 45);
+            this.btnConnect.TabIndex = 5;
+            this.btnConnect.Text = "Connect And Use This Token";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(334, 12);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(262, 22);
+            this.txtEmail.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(256, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 17);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Email";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(256, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 17);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Password";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(334, 40);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(262, 22);
+            this.txtPassword.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(675, 12);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(111, 17);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Company Token";
+            // 
+            // txtToken
+            // 
+            this.txtToken.Location = new System.Drawing.Point(809, 12);
+            this.txtToken.Name = "txtToken";
+            this.txtToken.Size = new System.Drawing.Size(357, 22);
+            this.txtToken.TabIndex = 10;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(675, 40);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(82, 17);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Filter Query";
+            // 
+            // txtFilter
+            // 
+            this.txtFilter.Location = new System.Drawing.Point(809, 40);
+            this.txtFilter.Multiline = true;
+            this.txtFilter.Name = "txtFilter";
+            this.txtFilter.Size = new System.Drawing.Size(357, 22);
+            this.txtFilter.TabIndex = 12;
+            this.txtFilter.Text = "Name Like \'%A%\'";
+            // 
+            // frmSample
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1178, 568);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtFilter);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtToken);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.btnSaveSelected);
+            this.Controls.Add(this.btnSaveAll);
+            this.Controls.Add(this.btnViewAcounts);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnViewItems);
+            this.Name = "frmSample";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Sample Form";
+            this.Load += new System.EventHandler(this.frmSample_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnViewItems;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnViewAcounts;
+        private System.Windows.Forms.Button btnSaveAll;
+        private System.Windows.Forms.Button btnSaveSelected;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtToken;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtFilter;
+    }
+}
+
---
+++ Pepperi.Demo.WinForms/frmSample.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Pepperi.SDK;
+using Pepperi.SDK.Helpers;
+using Pepperi.SDK.Model;
+using Pepperi.SDK.Model.Fixed;
+
+namespace WindowsFormsApp1
+{
+    public partial class frmSample : Form
+    {
+        private ApiClient client;
+        private PrivateAuthentication auth;
+        private List<Item> items;
+        private List<Account> accounts;
+        private string DeveloperKey = "8JZNRlMG2gFeudl51qNFHOMGPSV0XXPP";
+        private string CompanyToken = "";
+        private string APIBaseAddress = "https://api.pepperi.com/v1.0/";
+        PepperiLogger Logger = new PepperiLogger();
+
+
+        public frmSample()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {      
+            items = (List<Item>)client.Items.Find(txtFilter.Text);
+            dataGridView1.DataSource = items;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            accounts = (List<Account>)client.Accounts.Find(txtFilter.Text);
+            dataGridView1.DataSource = accounts;
+        }
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var fields = new List<string>();
+            fields.Add("ExternalID");
+            fields.Add("MainCategoryID");
+            fields.Add("Price");
+            var response = client.Items.BulkUpload(items, eOverwriteMethod.none, eBulkUploadMethod.Zip, fields, true);
+            var responseInfo = client.Items.WaitForBulkJobToComplete(response.JobID);
+            MessageBox.Show(responseInfo.Status);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            var index = dataGridView1.CurrentRow.Index;
+            var item = items[index];
+            var updatedItme = client.Items.Upsert(item);
+            MessageBox.Show("done!");
+
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var token = PrivateAuthentication.GetAPITokenData(APIBaseAddress, DeveloperKey, txtEmail.Text, txtPassword.Text, Logger);
+                if (token.APIToken == Guid.Empty.ToString())
+                    throw new Exception("user or passsword are incorrect.");
+                txtToken.Text = token.APIToken;
+                System.IO.File.WriteAllText(AppDomain.CurrentDomain.BaseDirectory + "token.txt", token.APIToken);
+                auth = new PrivateAuthentication(DeveloperKey, token.APIToken);
+                client = new ApiClient(APIBaseAddress, auth, Logger);
+                MessageBox.Show("Connected!");
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Not Connected!. reason: " + ex.ToString ());
+            }
+        }
+
+        private void frmSample_Load(object sender, EventArgs e)
+        {
+            if (System.IO.File.Exists(AppDomain.CurrentDomain.BaseDirectory + "token.txt"))
+            {
+                txtToken.Text = System.IO.File.ReadAllText(AppDomain.CurrentDomain.BaseDirectory + "token.txt");
+                CompanyToken = txtToken.Text;
+                auth = new Pepperi.SDK.PrivateAuthentication(DeveloperKey, CompanyToken);
+                client = new ApiClient(APIBaseAddress, auth, new PepperiLogger());
+            }
+        }
+    }
+
+
+}
